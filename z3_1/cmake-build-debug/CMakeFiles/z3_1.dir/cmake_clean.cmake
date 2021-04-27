c93193aecfb57c94600e8d63403ed11f14a856b7
Kateika-M <muromskaya.katya@mail.ru>
2021-04-28 02:13:49 +0300

    making codes

---
+++ z3_1/cmake-build-debug/CMakeFiles/z3_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/z3_1.dir/main.cpp.o"
+  "z3_1"
+  "z3_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/z3_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ z3_2/cmake-build-debug/CMakeFiles/z3_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/z3_2.dir/main.cpp.o"
+  "z3_2"
+  "z3_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/z3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
